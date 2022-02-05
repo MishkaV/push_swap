cb3b9b4af2bdc1a355aa4a2427283593f2ee8997
Михаил Ворожцов <MAVorozhtsov@sberbank.ru>
2022-02-05 12:02:41 +0300

    add checking null

--- srcs/operations2.c
+++ srcs/operations2.c
@@ -6,7 +6,7 @@
 /*   By: jbenjy <jbenjy@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 11:44:55 by jbenjy            #+#    #+#             */
-/*   Updated: 2022/02/05 11:57:57 by jbenjy           ###   ########.fr       */
+/*   Updated: 2022/02/05 12:02:19 by jbenjy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_node* ra(t_node *root)
 {
+	if (!root)
+		return (0);
 	root = rotate(root);
 	write(STANDART_OUTPUT, "ra\n", 3);
 	return (root);
@@ -21,6 +23,8 @@ t_node* ra(t_node *root)
 
 t_node* rb(t_node *root)
 {
+	if (!root)
+		return (0);
 	root = rotate(root);
 	write(STANDART_OUTPUT, "rb\n", 3);
 	return (root);
@@ -28,6 +32,8 @@ t_node* rb(t_node *root)
 
 void	rr(t_node **root_a, t_node **root_b)
 {
+	if (!(*root_a) || !(*root_b))
+		return ;
 	*root_a = rotate(*root_a);
 	*root_b = rotate(*root_b);
 	write(STANDART_OUTPUT, "rr\n", 3);
--- srcs/operations3.c
+++ srcs/operations3.c
@@ -6,7 +6,7 @@
 /*   By: jbenjy <jbenjy@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/05 11:51:16 by jbenjy            #+#    #+#             */
-/*   Updated: 2022/02/05 11:57:44 by jbenjy           ###   ########.fr       */
+/*   Updated: 2022/02/05 12:02:16 by jbenjy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_node* rra(t_node *root)
 {
+	if (!root)
+		return (0);
 	root = reverse_rotate(root);
 	write(STANDART_OUTPUT, "rra\n", 4);
 	return (root);
@@ -21,6 +23,8 @@ t_node* rra(t_node *root)
 
 t_node* rrb(t_node *root)
 {
+	if (!root)
+		return (0);
 	root = reverse_rotate(root);
 	write(STANDART_OUTPUT, "rrb\n", 4);
 	return (root);
@@ -28,6 +32,8 @@ t_node* rrb(t_node *root)
 
 void	rrr(t_node **root_a, t_node **root_b)
 {
+	if (!(*root_a) || !(*root_b))
+		return ;
 	*root_a = reverse_rotate(*root_a);
 	*root_b = reverse_rotate(*root_b);
 	write(STANDART_OUTPUT, "rrr\n", 4);

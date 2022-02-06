040895993998dcc9ce88251b2a1a4e578eb277d4
Михаил Ворожцов <MAVorozhtsov@sberbank.ru>
2022-02-06 20:56:52 +0300

    delete comments

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: jbenjy <jbenjy@student.21-school.ru>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/02 17:25:01 by jbenjy            #+#    #+#             */
-/*   Updated: 2022/02/06 20:31:17 by jbenjy           ###   ########.fr       */
+/*   Updated: 2022/02/06 20:56:37 by jbenjy           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,8 @@ int main(int argc, char **argv)
         exit(1);
     is_valid_argv(argc, argv);
     parser(&all, argc, argv);
-    // print_dequeue(all.root_a);
-    // printf("|%d %d|\n", all.len_a, is_sorted_ascending(all.root_a));
     if (!is_sorted_ascending(all.root_a))
         sort_dequeu(&all);
-    // print_dequeue(all.root_a);
     clean_dequeu(all.root_a);
     clean_dequeu(all.root_b);
     return (0);
